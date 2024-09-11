7a5d925706bbe29de57b685c8a25fa0df870e4a9
Andre Luiz <andreluiz.jpofi@gmail.com>
2024-09-11 12:46:42 -0300

    BUG FIX: Freez when get a heavy backup

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -5,6 +5,7 @@ import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -27,31 +28,69 @@ public class MenuController implements Initializable{
     private Button btnStart;
 
     @FXML
+    private Button btnPause;
+
+    @FXML
     private TextArea displayConsole;
-    
-    Thread backupRun = new Thread() {};
+
+    private boolean isBackupRunning = false;
 
     @FXML
     public void OnBtnStartAction(){
-
-            if (backupRun.isAlive()) {
+            if (isBackupRunning) {
                 System.out.println("Already running...");
                 return;
             }
-            //Here, we need to create a new Thread every backup, because if we reutilize same Thread, we get a IllegalThreadStateException
-            backupRun = new Thread() {
-                @Override
-                public void run() {
-                        runBackup();
-                };
-            };
+            String sourceStringURL = sourceField.getText();
+            String destinyStringURL = destinyField.getText();
+            String ignoreStringURL = ignoreField.getText();
 
-            backupRun.start();
+            //Here, we need to create a new Thread every backup, because if we reutilize same Thread, we get a IllegalThreadStateException
+
+            Task<Void> backupTask = new Task<Void>() {
+                @Override
+                protected Void call() {
+                    try {
+                        Backup.run(sourceStringURL, destinyStringURL, ignoreStringURL);
+                        System.out.println("Backup Concluido com sucesso");
+                    } catch (BackupException e) {
+                        System.err.printf("\nError: "+e.getMessage() + "\n" + "Cause: " + e.getCause());
+                    }
+                    isBackupRunning = false;
+                    return null;
+                }
+            };
+            
+            new Thread(backupTask).start();
+            isBackupRunning = true;
+            
             return;
     }
     
-    private void redirectSystemStreams() {
+    @FXML
+    public void OnBtnPauseAction(){
+        // if (backupRun.isAlive()) {
+        //     try {
+        //         synchronized (backupRun){
+        //             System.out.println("Paused...");
+        //             backupRun.wait(1000);
+        //         }
+        //     } catch (Exception e) {
+        //         System.err.printf("\nError: "+e.getMessage() + "\n" + "Cause: " + e.getCause());
+        //     }
 
+        //     return;
+        // }    
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        displayConsole.setEditable(false);
+        redirectSystemStreams();
+        System.out.println("Ready");
+    }
+
+    private void redirectSystemStreams() {
         // Define o PrintStream personalizado para logs
         /*
          * O que está acontecendo nesse código?
@@ -59,36 +98,28 @@ public class MenuController implements Initializable{
          * O que estamos fazendo é sobreescrevendo o métodos e coloco ele dentro do textarea
         */
         PrintStream logStream = new PrintStream(new OutputStream() {
+            private StringBuilder buffer = new StringBuilder();
             @Override
             public void write(int b) {
-                displayConsole.appendText(String.valueOf((char) b));
+                char c = (char) b;
+                buffer.append(c);
+                displayConsole.appendText(buffer.toString());
             }
 
             @Override
             public void write(byte[] b, int off, int len) {
-                displayConsole.appendText(new String(b, off, len));
+                String str = new String(b, off, len);
+                buffer.append(str);
+                while (true) {
+                    int newlineIndex = buffer.indexOf("\n");
+                    if (newlineIndex == -1) break;
+                    displayConsole.appendText(buffer.substring(0, newlineIndex + 1));
+                    buffer.delete(0, newlineIndex + 1);
+                }
             }
         });
-
         System.setOut(logStream);
         System.setErr(logStream);
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        // redirectSystemStreams();
-        System.out.println("Ready");
-    }
-
-    private void runBackup (){
-        String sourceStringURL = sourceField.getText();
-        String destinyStringURL = destinyField.getText();
-        String ignoreStringURL = ignoreField.getText();
-        try {
-            Backup.run(sourceStringURL, destinyStringURL, ignoreStringURL);
-            System.out.println("...");
-        } catch (BackupException e) {
-            System.err.printf("\n"+e.getMessage() + "\n" + "Cause: " + e.getCause());
-        }
-    }
 }
